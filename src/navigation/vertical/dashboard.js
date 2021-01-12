09c290552bbaa6d25b4fed6459c42cb4912493cb
Bryan Allebone <@af.milpm2>
2021-01-12 14:35:34 -0600

    Try That

--- src/navigation/vertical/dashboard.js
+++ src/navigation/vertical/dashboard.js
@@ -9,18 +9,15 @@ export default [
       {
         title: 'Mobile Program',
         route: 'dashboard-ecommerce',
-        icon: 'TabletIcon',
       },
       {
         title: 'OMNI Analytics',
         route: 'dashboard-analytics',
-        icon: 'ActivityIcon',
       },
       {
         title: 'Readiness Assessment',
         route: 'dashboard-readiness',
-        icon: 'ActivityIcon',
       },
     ],
-  }
+  },
 ]
