c1ad6b6dd8ddec5eb33756904a70922ded0ea655
Bryan Allebone <@af.milpm2>
2021-01-21 11:29:05 -0600

    New Look for Dashboard

--- src/navigation/vertical/dashboard.js
+++ src/navigation/vertical/dashboard.js
@@ -1,26 +1,9 @@
 export default [
   {
-    title: 'Dashboards',
+    title: 'Home',
     icon: 'HomeIcon',
+    route: 'dashboard-personal',
     tag: '4',
     tagVariant: 'light-warning',
-    children: [
-      {
-        title: 'Personal',
-        route: 'dashboard-personal',
-      },
-      {
-        title: 'Mobile',
-        route: 'dashboard-ecommerce',
-      },
-      {
-        title: 'Analytics',
-        route: 'dashboard-analytics',
-      },
-      {
-        title: 'Readiness',
-        route: 'dashboard-readiness',
-      },
-    ],
   },
 ]
