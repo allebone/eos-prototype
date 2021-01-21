538cd7920a59134109e592e9b46f6c41da4dce41
Bryan Allebone <@af.milpm2>
2021-01-21 12:02:55 -0600

    fixes

--- src/navigation/vertical/index.js
+++ src/navigation/vertical/index.js
@@ -35,7 +35,7 @@ import users from './users'
 import mobile from './mobile'
 import supply from './supply'
 import aircraft from './aircraft'
-import formAndTable from './forms-and-table'
+// import formAndTable from './forms-and-table'
 
 // Array of sections
 export default [...dashboard, ...mobile, ...supply, ...aircraft, ...users]
