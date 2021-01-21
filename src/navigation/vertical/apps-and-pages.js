32b9d23e9904e78ca19ceca30a9f4d01f3612d39
Bryan Allebone <@af.milpm2>
2021-01-21 09:31:31 -0600

    Removing Extras

--- src/navigation/vertical/apps-and-pages.js
+++ src/navigation/vertical/apps-and-pages.js
@@ -3,28 +3,6 @@ export default [
     header: 'Apps & Pages',
   },
   {
-    title: 'Invoice',
-    icon: 'FileTextIcon',
-    children: [
-      {
-        title: 'List',
-        route: 'apps-invoice-list',
-      },
-      {
-        title: 'Preview',
-        route: { name: 'apps-invoice-preview', params: { id: 4987 } },
-      },
-      {
-        title: 'Edit',
-        route: { name: 'apps-invoice-edit', params: { id: 4987 } },
-      },
-      {
-        title: 'Add',
-        route: { name: 'apps-invoice-add' },
-      },
-    ],
-  },
-  {
     title: 'Users',
     icon: 'UserIcon',
     children: [
@@ -41,99 +19,5 @@ export default [
         route: { name: 'apps-users-edit', params: { id: 21 } },
       },
     ],
-  },
-  {
-    title: 'Pages',
-    icon: 'FileIcon',
-    children: [
-      {
-        title: 'Authentication',
-        icon: 'CircleIcon',
-        children: [
-          {
-            title: 'Login v1',
-            route: 'auth-login-v1',
-            target: '_blank',
-          },
-          {
-            title: 'Login v2',
-            route: 'auth-login-v2',
-            target: '_blank',
-          },
-          {
-            title: 'Register v1',
-            route: 'auth-register-v1',
-            target: '_blank',
-          },
-          {
-            title: 'Register v2',
-            route: 'auth-register-v2',
-            target: '_blank',
-          },
-          {
-            title: 'Forgot Password v1',
-            route: 'auth-forgot-password-v1',
-            target: '_blank',
-          },
-          {
-            title: 'Forgot Password v2',
-            route: 'auth-forgot-password-v2',
-            target: '_blank',
-          },
-          {
-            title: 'Reset Password v1',
-            route: 'auth-reset-password-v1',
-            target: '_blank',
-          },
-          {
-            title: 'Reset Password v2',
-            route: 'auth-reset-password-v2',
-            target: '_blank',
-          },
-        ],
-      },
-      {
-        title: 'Account Settings',
-        route: 'pages-account-setting',
-      },
-      {
-        title: 'Profile',
-        route: 'pages-profile',
-      },
-      {
-        title: 'Faq',
-        route: 'pages-faq',
-      },
-      {
-        title: 'Knowledge Base',
-        route: 'pages-knowledge-base',
-      },
-      {
-        title: 'Miscellaneous',
-        icon: 'CircleIcon',
-        children: [
-          {
-            title: 'Coming Soon',
-            route: 'misc-coming-soon',
-            target: '_blank',
-          },
-          {
-            title: 'Not Authorized',
-            route: 'misc-not-authorized',
-            target: '_blank',
-          },
-          {
-            title: 'Under Maintenance',
-            route: 'misc-under-maintenance',
-            target: '_blank',
-          },
-          {
-            title: 'Error',
-            route: 'misc-error',
-            target: '_blank',
-          },
-        ],
-      },
-    ],
-  },
+  }
 ]
